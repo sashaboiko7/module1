6c904e378ef9ec7c0255bf07fcbb056d9b5b6ad0
Oleksandra <oleksandraboiko777@gmail.com>
2024-04-05 10:38:40 +0300

    add write_to_file function

--- text-utils/text_utilites.py
+++ text-utils/text_utilites.py
@@ -6,3 +6,11 @@ def read_file(input_file):
     except FileNotFoundError:
         print(f"Файл '{input_file}' не знайдено.")
         return None
+
+def write_to_file(output_file, filtered_lines):
+    try:
+        with open(output_file, 'w') as f:
+            f.writelines(filtered_lines)
+        print(f"Результат фільтрації був записаний у файл '{output_file}'.")
+    except IOError:
+        print(f"Помилка при записі у файл '{output_file}'.")
