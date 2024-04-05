c7217dbee4968453b3f8839db4b55013f0c28c87
Oleksandra <oleksandraboiko777@gmail.com>
2024-04-05 10:48:14 +0300

    update text_filter function

--- services/text_filter.py
+++ services/text_filter.py
@@ -1,3 +1,3 @@
 def filter_lines(lines, keyword):
-    filtered_lines = [line for line in lines if keyword in line]
+    filtered_lines = [line for line in lines if f' {keyword} ' in line or line.startswith(f'{keyword} ') or line.endswith(f' {keyword}') or line == keyword]
     return filtered_lines
