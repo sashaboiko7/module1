4a396624f43ca82c405f08a7f907e72b19db76b1
Oleksandra <oleksandraboiko777@gmail.com>
2024-04-05 11:47:17 +0300

    add documentation

--- text_utils/text_utilites.py
+++ text_utils/text_utilites.py
@@ -1,4 +1,9 @@
 def read_file(input_file):
+    """
+    function attempts to open and read the contents of a specified file.
+    If the file is found and read successfully, it returns a list of lines from
+    the file. If the file is not found, it prints an error message and returns None.
+    """
     try:
         with open(input_file, 'r') as f:
             lines = f.readlines()
@@ -7,7 +12,14 @@ def read_file(input_file):
         print(f"Файл '{input_file}' не знайдено.")
         return None
 
+
 def write_to_file(output_file, filtered_lines):
+    """
+    function attempts to write the contents of a list of lines to a specified file.
+    If the write operation is successful, it prints a success message. If an error
+    occurs while writing to the file, it prints an error message
+    """
+
     try:
         with open(output_file, 'w') as f:
             f.writelines(filtered_lines)
